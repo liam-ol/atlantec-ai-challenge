cec8c74c7995d0651300fb9ec25c4e65b47e6e61
Liam Ó Lionáird <liamolionaird@gmail.com>
2026-04-11 21:57:30 +0100

    okay coordinates are actually fixed now

--- datasets/new_dataset.py
+++ datasets/new_dataset.py
@@ -6,11 +6,10 @@ Author: Liam Ó Lionáird
 import geopandas
 
 with open("Gaeltacht_Boundaries_Generalised_100m.geojson") as gaeltacht_json, open("Settlements_Generalised_100m.geojson") as towns_json:
-    gaeltacht = geopandas.read_file(gaeltacht_json)
-    towns = geopandas.read_file(towns_json)
+    gaeltacht = geopandas.read_file(gaeltacht_json).to_crs(4326)
+    towns = geopandas.read_file(towns_json).to_crs(4326)
 
-towns.geometry = geopandas.points_from_xy(towns.EAST,towns.NORTH)
-towns.crs = "epsg:2157"
+
 
 gael_towns = towns.loc[towns.within(gaeltacht.union_all())]
 
